c83dcf94ecd9355f5ff180ef44867d0af5b0cae5
nuntteo <101611219+nuntteo@users.noreply.github.com>
2022-05-03 09:56:11 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+let capture;
+var circles = []
+var total = 10;
+
+
+
+function setup() {
+  createCanvas(600,600);
+  capture = createCapture(VIDEO);
+  capture.size(600, 600);
+  imageMode(CENTER);
+  background(0);
+  
+	for(var i = 0; i < total; i++){
+		circles[i] = {};
+		circles[i].prevPos = {x: width/2, y: height/2}
+		circles[i].pos = {x: width/2, y: height/2}
+		circles[i].dir = random() > 0.5 ? 1 : -1
+		circles[i].radius = random(1, 0)
+		circles[i].angle = 0
+	}
+}
+
+function draw() {
+	for(var i = 0; i < total; i++){
+		var circle = circles[i]
+		circle.angle += 1 / circle.radius * circle.dir
+
+		circle.pos.x += cos(circle.angle) * circle.radius
+		circle.pos.y += sin(circle.angle) * circle.radius
+		if(brightness(capture.get(round(circle.pos.x), round(circle.pos.y))) > 0 || circle.pos.x < 0 || circle.pos.x > width || circle.pos.y < 0 || circle.pos.y > height){
+			circle.dir *= -1
+			circle.radius = random(1, 10)
+			circle.angle += PI
+		}
+      
+		stroke(capture.get(circle.pos.x, circle.pos.y))
+		line(circle.prevPos.x, circle.prevPos.y, circle.pos.x, circle.pos.y)
+      strokeWeight(1);
+
+		//circle.prevPos.x = circle.pos.x
+		//circle.prevPos.y = circle.pos.y
+	}
+
+}
